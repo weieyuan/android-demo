9e8567b5fcdcc2a99e4ac6313bd30654125c8f2a
危鄂元 <weieyuan@yeah.net>
2018-10-23 23:32:44 +0800

    新增页面跳转功能，增加备忘录界面

--- app/src/main/java/com/example/weieyuan/weiapp/MainActivity.java
+++ app/src/main/java/com/example/weieyuan/weiapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.weieyuan.weiapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 绑定button的点击事件
+        this.bindBtnEvent();
+    }
+
+    private void bindBtnEvent() {
+        Button b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MemoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
