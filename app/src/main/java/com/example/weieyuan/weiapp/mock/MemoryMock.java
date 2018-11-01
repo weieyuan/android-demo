5d604b17c8c8b5c22361a41b694742dac879f10c
危鄂元 <weieyuan@yeah.net>
2018-11-01 23:06:30 +0800

    修改备忘录页面展示的样式

--- app/src/main/java/com/example/weieyuan/weiapp/mock/MemoryMock.java
+++ app/src/main/java/com/example/weieyuan/weiapp/mock/MemoryMock.java
@@ -13,7 +13,7 @@ public class MemoryMock {
 
     public static List<MemoryItemModel> getMockData() {
         List<MemoryItemModel> data = new ArrayList<>();
-        for(int i = 0; i < 40; i++) {
+        for(int i = 0; i < 15; i++) {
             MemoryItemModel model = new MemoryItemModel("2018-10-18", "以产品的形态做技术，以创业的心态做产品。要证明自己的价值，要将自己的能力体现在产品中。");
             data.add(model);
         }
