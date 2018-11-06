05a858eb5b65956333cbb060b2450d279f0b366e
危鄂元 <weieyuan@yeah.net>
2018-11-06 23:22:10 +0800

    新增创建新的备忘录的页面

--- app/src/main/java/com/example/weieyuan/weiapp/MemoryActivity.java
+++ app/src/main/java/com/example/weieyuan/weiapp/MemoryActivity.java
@@ -1,9 +1,12 @@
 package com.example.weieyuan.weiapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.weieyuan.weiapp.model.CustomerAdapter;
@@ -17,6 +20,8 @@ public class MemoryActivity extends AppCompatActivity {
 
     private ListView listView;
 
+    private Button createBtn;
+
     private List<MemoryItemModel> data = new ArrayList<>();
 
     private MemoryService service = new MemoryService();
@@ -28,11 +33,24 @@ public class MemoryActivity extends AppCompatActivity {
 
         this.listView = findViewById(R.id.listView);
         initListView();
+
+        this.createBtn = findViewById(R.id.button3);
+        initCreateBtn();
     }
 
     private void initListView() {
         this.data.addAll(this.service.getData());
         BaseAdapter adapter = new CustomerAdapter(this, this.data);
         this.listView.setAdapter(adapter);
+    }
+
+    private void initCreateBtn() {
+        this.createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MemoryActivity.this, NewMemoryActivity.class);
+                MemoryActivity.this.startActivity(intent);
+            }
+        });
     }
 }
